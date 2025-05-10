d971004600155b27801d8ec4af997d30678abce8
Thomas <thomasn8255@gmail.com>
2025-05-10 01:01:48 -0700

    adding table of contents

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  useLocation,
+} from "react-router-dom";
 
 const Header = () => {
   return (
@@ -12,20 +19,75 @@ const Header = () => {
   );
 };
 
+const Intro = () => {
+  return (
+    <div className="intro">
+      <Header />
+      <Question />
+    </div>
+  );
+};
+
 const Question = () => {
   return (
-    <h1>
+    <NavLink to="/toc" className="toc">
+      {" "}
       Should U.S. taxpayers fund expensive space telescopes like JWST while
       there are urgent issues on Earth?
-    </h1>
+    </NavLink>
   );
+};
+
+const AppContent = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Intro />} />
+      <Route path="/topic1" element={<Topic1 />} />
+      <Route path="/topic2" element={<Topic2 />} />
+      <Route path="/topic3" element={<Topic3 />} />
+      <Route path="/Conclusion" element={<Conclusion />} />
+    </Routes>
+  );
+};
+
+const Contents = () => {
+  return (
+    <div className="contents">
+      Table of Contents
+      <div className="topics">
+        <NavLink to="/topic1">Scientific and Technological Impact</NavLink>
+        <NavLink to="/topic2">
+          Historical Precedent and Long-Term Vision
+        </NavLink>
+        <NavLink to="/topic3">Ethics, Cost, and Public Responsibility</NavLink>
+        <NavLink to="/conclusion">Conclusion</NavLink>
+      </div>
+    </div>
+  );
+};
+
+const Topic1 = () => {
+  return <></>;
+};
+
+const Topic2 = () => {
+  return <></>;
+};
+
+const Topic3 = () => {
+  return <></>;
+};
+
+const Conclusion = () => {
+  return <></>;
 };
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <Question />
+      <Router>
+        <AppContent />
+      </Router>
     </div>
   );
 };
